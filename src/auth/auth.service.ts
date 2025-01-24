1dab5a581d34ba93e74653200c5de5460b88ea29
Alexsandro Moraes de Brito <lexfisica@gmail.com>
2025-01-24 11:36:58 -0400

    feat: created authentication, but without token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
+import {UserService} from "../user/user.service";
+import {Prisma, User} from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    @Inject()
+    private readonly userService: UserService;
+
+    async signin(params: Prisma.UserCreateInput): Promise<Omit<User, 'password'>> {
+        const user = await this.userService.user(
+            {
+                email: params.email
+            });
+        if (!user) throw new NotFoundException('User not found');
+        const passwordMatch = await bcrypt.compare(
+            params.password,
+            user.password,
+        );
+        if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+        const { password, ...result } = user;
+
+        return result;
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,6 +6,7 @@ import {DatabaseModule} from "../database/database.module";
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
